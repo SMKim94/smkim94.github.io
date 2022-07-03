03bcde06c18fb5295c2811e05605c4cbce84c8a8
SMKim94 <floor3th@gmail.com>
2022-07-03 16:50:00 +0900

    change react version 16.12.0 -> 18.2.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import randomColor from 'randomcolor';
+import { ReactComponent as Logo } from 'assets/images/svg/logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [logoColor, setLogoColor] = useState('#61DAFB');
+
+	const changeLogoColor = (e) => {
+		e.preventDefault();
+		const color = randomColor();
+		setLogoColor(color);
+	}
+
+	return (
+		<div className="App">
+			<Logo fill={logoColor} onClick={changeLogoColor} onMouseMove={changeLogoColor}/>
+		</div>
+	);
 }
 
 export default App;
