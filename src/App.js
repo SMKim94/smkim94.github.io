38b8b554a3ae29f576030485170aa50c94cc418a
SMKim94 <floor3th@gmail.com>
2022-07-03 17:38:01 +0900

    change main page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import { ReactComponent as Logo } from 'assets/images/svg/logo.svg';
 
 const App = () => {
 	const [logoColor, setLogoColor] = useState('#61DAFB');
+	const [textCount, setTextCount] = useState(10);
 
-	const changeLogoColor = (e) => {
-		e.preventDefault();
+	const makeRandomStyle = () => {
+		return {
+			position: 'absolute',
+			top: `${Math.min(Math.round(Math.random() * 60), 80)}%`,
+			left: `${Math.min(Math.round(Math.random() * 60), 80)}%`,
+			fontSize: `${Math.round(Math.random() * 10)}rem`,
+			color: randomColor(),
+		}
+	}
+
+	const changeLogoColor = () => {
 		const color = randomColor();
 		setLogoColor(color);
 	}
 
+	const changeRandom = (e) => {
+		e.preventDefault();
+		changeLogoColor();
+		setTextCount(Math.round(Math.random() * 50));
+	}
+
 	return (
-		<div className="App">
-			<Logo fill={logoColor} onClick={changeLogoColor} onMouseMove={changeLogoColor}/>
+		<div className="App" style={{ height: '100vh', overflow: 'hidden' }} onClick={changeRandom} onMouseMove={changeRandom}>
+			{Array(textCount).fill(0).map((_, i) => <span key={i} style={makeRandomStyle()}>준비중입니다.</span>)}
 		</div>
 	);
 }
